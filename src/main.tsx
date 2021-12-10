756309cfe706a787f2840f21c6001947473b5fb1
Hans Geel <mcmacker4@gmail.com>
2021-12-10 12:39:21 +0100

    Add redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,36 @@
 import { render, h } from 'preact'
-import { useState, useCallback } from 'preact/hooks'
+import { useCallback } from 'preact/hooks'
+import { Provider } from 'react-redux'
+import {useAppDispatch, useAppSelector} from './state/hooks'
+import { increment } from './state/slice/counter'
+
+import { store } from './state/store'
 
 import './style.css'
 
-function Application() {
-    const [count, setCount] = useState(0)
 
-    debugger;
+function Counter() {
+    const count = useAppSelector(state => state.counter.value)
+    const dispatch = useAppDispatch()
 
-    const plusOne = useCallback(() => {
-        setCount(count + 1)
+    const incrementCallback = useCallback(() => {
+        dispatch(increment())
     }, [count])
 
     return (
         <div class="content">
             <span>Count: {count}</span>
-            <a class="button" onClick={plusOne}>One More!</a>
+            <a class="button" onClick={incrementCallback}>One More!</a>
         </div>
+    )
+}
+
+
+function Application() {
+    return (
+        <Provider store={store}>
+            <Counter />
+        </Provider>
     )
 }
 
